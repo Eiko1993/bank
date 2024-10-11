2feb599d776643fd9d280bbd9b8a27698dced93c
Vincent Merah <vincent.merah@gmail.com>
2024-10-11 09:52:06 +0200

    log out beginning

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import logo from "../Assets/img/argentBankLogo.png";
 
 
 /*const logChange = document.querySelector(".main-nav-item");*/
+/*let user = window.localStorage.getItem("user");*/
+
 
 
 /*Modifier page*/
 
 /*if (localStorage.getItem('token')) {
-    logChange.innerText = "logout";
+    logChange.innerText = "Sign Out";
     logChange.addEventListener("click", ()=>{
         localStorage.removeItem("token");
-        window.location.href = "index.html";
+        window.location.href = "/";
+        logChange.innerText= "Sign In";
     })
-
 }*/
 
 function Header(){
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Track if user is logged in
+    const navigate = useNavigate();
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+          setIsLoggedIn(true);
+        }
+      }, []);
+    
+      const handleSignOut = () => {
 
+        localStorage.removeItem('token');
+        setIsLoggedIn(false);
+        navigate('/'); 
+      };  
    return(
     <header>
         <nav className="main-nav">
@@ -27,10 +44,16 @@ function Header(){
         <h1 className="sr-only">Argent Bank</h1>
         </Link>
         <div>
+        {isLoggedIn ? (
+          <button className="main-nav-item" onClick={handleSignOut}>
+            Sign Out
+          </button>
+        ) : (
             <Link to="/sign-in" className="main-nav-item">
             <i className="fa fa-user-circle"></i>
             Sign In
         </Link>
+        )}
         </div>
     </nav>
   </header>
