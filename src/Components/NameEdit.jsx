0135006d81ea44ab297c90427dbc9730a3b6b186
Vincent Merah <vincent.merah@gmail.com>
2024-10-28 16:02:01 +0100

    name edit

--- src/Components/NameEdit.jsx
+++ src/Components/NameEdit.jsx
@@ -3,15 +3,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import {editUsername, fetchUserProfile} from "../redux/editReducer";
 
 function NameEdit() {
-const [newUsername, setNewUsername] = useState(''); 
+  const [newUsername, setNewUsername] = useState(''); 
   const dispatch = useDispatch();
   const { loading, error, user } = useSelector((state) => state.profile);
-  
-  
+  const [editName, seteditName] = useState(false);
+
+  const displayContent = () => {
+    seteditName(!editName);
+  };
+
   useEffect(() => {
     dispatch(fetchUserProfile());
   }, [dispatch]);
-  
+
   useEffect(() => {
     console.log("Fetched user:", user); // Debugging line
     if (user && user.userName) {
@@ -22,50 +26,61 @@ const [newUsername, setNewUsername] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(editUsername(newUsername)).then((result) => {
-        if (!result.error) {
-            alert("Username updated successfully")
-        }
+      if (!result.error) {
+        alert("Username updated successfully");
+        seteditName(false);
+      }
     });
   };
 
   const handleCancel = (e) => {
     e.preventDefault();
     setNewUsername(user.userName); // Reset to the original username
+    seteditName(false);
   };
 
-  return(
+  return (
     <div>
-      <>
-        <section className='edit-content'>
-            <h1>Edit user info</h1>
-            <form onSubmit={handleSubmit}>
+      <section className='edit-content'>
+        <div>
+        {!editName && (
+          <>
+            <h3>Welcome back, {user?.userName}</h3>
+            <button className='edit-button' onClick={displayContent}>Edit</button>
+          </>
+        )}
+          {editName && (
+            <>
+              <h1>Edit user info</h1>
+              <form onSubmit={handleSubmit}>
                 <div className='input-wrapper-edit'>
-                    <label htmlFor='username'>User name: </label>
-                    <input type='text' id='username' value={newUsername} onChange={(e) => setNewUsername(e.target.value)} placeholder={user?.userName} />
+                  <label htmlFor='username'>User name: </label>
+                  <input type='text' id='username' value={newUsername} onChange={(e) => setNewUsername(e.target.value)} placeholder={user?.userName} />
                 </div>
                 <div className='input-wrapper-edit'>
-                    <label htmlFor='firstname'>First name: </label>
-                    <input type='text' id='firstname' value={user?.firstName} placeholder={user?.firstName} disabled />
+                  <label htmlFor='firstname'>First name: </label>
+                  <input type='text' id='firstname' value={user?.firstName} placeholder={user?.firstName} disabled />
                 </div>
                 <div className='input-wrapper-edit'>
-                    <label htmlFor='lastname'>Last name: </label>
-                    <input type='text' id='lastname' value={user?.lastName} placeholder={user?.lastName} disabled />
+                  <label htmlFor='lastname'>Last name: </label>
+                  <input type='text' id='lastname' value={user?.lastName} placeholder={user?.lastName} disabled />
                 </div>
                 <div className='input-wrapper-edit'>
-                <button className="edit-button" type="submit" disabled={loading}>
-                {loading ? 'Saving...' : 'Save'}                
-                </button>
-                <button className="edit-button" onClick={handleCancel} disabled={loading}>Cancel
-                </button>
+                  <button className="edit-button" type="submit" disabled={loading}>
+                    {loading ? 'Saving...' : 'Save'}
+                  </button>
+                  <button className="edit-button" onClick={handleCancel} disabled={loading}>Cancel</button>
                 </div>
-                {error&&(
-                    <div className='error-message'>{error}</div>
+                {error && (
+                  <div className='error-message'>{error}</div>
                 )}
-            </form>
-        </section>
-        </>
+              </form>
+            </>
+          )}
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
 export default NameEdit;
